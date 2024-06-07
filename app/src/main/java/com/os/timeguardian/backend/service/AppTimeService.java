90c42029e11d04f715729fbac1e60c3505f8f4e9
Luca Gloor <glooorluca@gmail.com>
2024-06-07 17:09:42 +0200

    Adjust AppTimeService

--- app/src/main/java/com/os/timeguardian/backend/service/AppTimeService.java
+++ app/src/main/java/com/os/timeguardian/backend/service/AppTimeService.java
@@ -1,14 +1,8 @@
 package com.os.timeguardian.backend.service;
 
-import static android.app.usage.UsageStatsManager.INTERVAL_MONTHLY;
-import static android.app.usage.UsageStatsManager.INTERVAL_WEEKLY;
-import static android.app.usage.UsageStatsManager.INTERVAL_YEARLY;
-import static android.content.ContentValues.TAG;
-
 import android.app.Service;
 import android.app.usage.UsageEvents;
 import android.app.usage.UsageEvents.Event;
-import android.app.usage.UsageStats;
 import android.app.usage.UsageStatsManager;
 import android.content.Context;
 import android.content.Intent;
@@ -16,11 +10,14 @@ import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
 import android.os.IBinder;
 import android.util.Log;
+import android.util.Pair;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.time.ZoneId;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -34,104 +31,125 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.stream.Collectors;
 
-public class AppTimeService extends Service{
-
+public class AppTimeService extends Service {
+    private static final long TEN_MINUTES = 10 * 60 * 60 * 1000L;
+    private static Pair<Long, Map<String, Long>> usageStatsTodayCache;
+    private static Pair<Long, List<Map<String, Long>>> usageStatsTodayGroupByHoursCache;
+    private static Pair<Long, List<Map<String, Long>>> usageStatsPastSevenDaysCache;
+    private static List<String> allPackageNames;
+    private static final String TAG = "AppTimeService";
     private final PackageManager packageManager;
     private final UsageStatsManager statsManager;
 
     public AppTimeService(Context context) {
         statsManager = (UsageStatsManager) context.getSystemService(USAGE_STATS_SERVICE);
         packageManager = context.getPackageManager();
+        allPackageNames = getAllPackageNames();
     }
 
-    public Map<String, Integer> getUsageStatsToday() {
-        long endTime = LocalDate.now()
-                .atTime(23, 59, 59)
-                .atZone(ZoneId.systemDefault())
-                .toInstant()
-                .toEpochMilli();
-        long startTime = LocalDate.now().minusDays(1)
-                .atStartOfDay()
-                .atZone(ZoneId.systemDefault())
-                .toInstant()
-                .toEpochMilli();
+    public Map<String, Long> getUsageStatsToday() {
+        if (usageStatsTodayCache != null
+                && usageStatsTodayCache.first >= System.currentTimeMillis() - TEN_MINUTES) {
+            return usageStatsTodayCache.second;
+        }
 
-        List<String> allPackageNames = getAllPackageNames();
-        return getUsageTimeForRange(allPackageNames, startTime, endTime);
+        LocalDate today = LocalDate.now();
+        long startTime = getStartTime(today.minusDays(1));
+        long endTime = getEndTime(today);
+        Map<String, Long> usageStatsToday = getUsageTimeForRange(startTime, endTime);
+        usageStatsTodayCache = new Pair<>(System.currentTimeMillis(), usageStatsToday);
+        return usageStatsToday;
     }
 
-    public List<Map<String, Integer>> getUsageStatsPastSevenDays() {
-        //TODO: Add Multi-threading
-        long stopWatchBegin = System.currentTimeMillis();
-        ArrayList<Future<Map<String, Integer>>> futures = new ArrayList<>(7);
-        ArrayList<Map<String, Integer>> weekList = new ArrayList<>(7);
-        LocalDate date = LocalDate.now();
+    public List<Map<String, Long>> getUsageStatsTodayGroupByHours() {
+        if (usageStatsTodayGroupByHoursCache != null
+                && usageStatsTodayGroupByHoursCache.first >= System.currentTimeMillis() - TEN_MINUTES) {
+            return usageStatsTodayGroupByHoursCache.second;
+        }
 
-        List<String> allPackageNames = getAllPackageNames();
+        int hoursToShow = LocalDateTime.now().getHour() + 1;
+        List<Future<Map<String, Long>>> futures = new ArrayList<>(hoursToShow);
+        LocalDateTime maxTime = LocalDateTime.of(LocalDate.now(), LocalTime.of(hoursToShow, 0));
 
-        ExecutorService executorService = Executors.newFixedThreadPool(7);
+        ExecutorService executorService = Executors.newFixedThreadPool(hoursToShow);
 
-        for (int i = 0; i < 7; i++) {
-            final long startTime = date.minusDays(i + 1)
-                    .atStartOfDay()
-                    .atZone(ZoneId.systemDefault())
-                    .toInstant()
-                    .toEpochMilli();
+        for (int i = 0; i < hoursToShow; i++) {
+            final long startTime = convertToEpochMilli(maxTime.minusHours(i + 1));
+            final long endTime =  convertToEpochMilli(maxTime.minusHours(i));
 
-            final long endTime = date.minusDays(i)
-                    .atTime(23, 59, 59)
-                    .atZone(ZoneId.systemDefault())
-                    .toInstant()
-                    .toEpochMilli();
-
-            Future<Map<String, Integer>> future = executorService.submit(() -> getUsageTimeForRange(allPackageNames, startTime, endTime));
-            futures.add(future);
+            Future<Map<String, Long>> future = executorService.submit(() -> getUsageTimeForRange(startTime, endTime));
+            futures.add(i, future);
         }
 
         executorService.shutdown();
 
-        for (Future<Map<String, Integer>> future : futures) {
-            try {
-                weekList.add(future.get());
-            } catch (ExecutionException | InterruptedException e) {
-                Log.e(TAG, "getUsageStatsPastSevenDays: ", e);
-                return Collections.emptyList();
-            }
+        List<Map<String, Long>> hourList = getValuesFromFutures(hoursToShow, futures);
+        usageStatsTodayGroupByHoursCache = new Pair<>(System.currentTimeMillis(), hourList);
+        return hourList;
+    }
+
+    public List<Map<String, Long>> getUsageStatsPastSevenDays() {
+        if (usageStatsPastSevenDaysCache != null
+                && usageStatsPastSevenDaysCache.first >= System.currentTimeMillis() - TEN_MINUTES) {
+            return usageStatsPastSevenDaysCache.second;
         }
 
-        long stopWatchEnd = System.currentTimeMillis();
-        System.out.println(stopWatchEnd - stopWatchBegin + " ms");
+        int numberOfDays = 7;
+        List<Future<Map<String, Long>>> futures = new ArrayList<>(numberOfDays);
+        LocalDate today = LocalDate.now();
+
+        ExecutorService executorService = Executors.newFixedThreadPool(numberOfDays);
+
+        for (int i = 0; i < numberOfDays; i++) {
+            final long startTime = getStartTime(today.minusDays(i + 1));
+            final long endTime = getEndTime(today.minusDays(i));
+
+            Future<Map<String, Long>> future = executorService.submit(() -> getUsageTimeForRange(startTime, endTime));
+            futures.add(i, future);
+        }
+
+        executorService.shutdown();
+
+        List<Map<String, Long>> weekList = getValuesFromFutures(numberOfDays, futures);
+        usageStatsPastSevenDaysCache = new Pair<>(System.currentTimeMillis(), weekList);
         return weekList;
     }
 
     @NonNull
-    private List<String> getAllPackageNames() {
-        return packageManager.getInstalledApplications(0)
-                .stream()
-                .filter(applicationInfo -> (applicationInfo.flags & ApplicationInfo.FLAG_SYSTEM) != 1)
-                .map(applicationInfo -> applicationInfo.packageName)
-                .collect(Collectors.toList());
+    private static List<Map<String, Long>> getValuesFromFutures(int hoursToShow, List<Future<Map<String, Long>>> futures) {
+        List<Map<String, Long>> statsList = new ArrayList<>(hoursToShow);
+        for (Future<Map<String, Long>> future : futures) {
+            try {
+                statsList.add(future.get());
+            } catch (ExecutionException | InterruptedException e) {
+                Log.e(AppTimeService.TAG, "Error getting value from Future: ", e);
+                statsList.add(Collections.emptyMap());
+            }
+        }
+        return statsList;
     }
 
-    public List<UsageStats> getUsageStatsWeek() {
-        long endTime = System.currentTimeMillis();
-        long startTime = endTime - 1000;
-        return statsManager.queryUsageStats(INTERVAL_WEEKLY, startTime, endTime);
+    private static long getStartTime(LocalDate date) {
+        return convertToEpochMilli(date.atStartOfDay());
     }
 
-    public List<UsageStats> getUsageStatsMonth() {
-        long endTime = System.currentTimeMillis();
-        long startTime = endTime - 1000;
-        return statsManager.queryUsageStats(INTERVAL_MONTHLY, startTime, endTime);
+    private static long getEndTime(LocalDate date) {
+        return convertToEpochMilli(date.atTime(23, 59, 59));
     }
 
-    public List<UsageStats> getUsageStatsYear() {
-        long endTime = System.currentTimeMillis();
-        long startTime = endTime - 1000;
-        return statsManager.queryUsageStats(INTERVAL_YEARLY, startTime, endTime);
+    private static long convertToEpochMilli(LocalDateTime localDateTime) {
+        return localDateTime.atZone(ZoneId.systemDefault()).toInstant().toEpochMilli();
     }
 
-    public Map<String, Integer> getUsageTimeForRange(List<String> packageNames, long startTime, long endTime) {
+    /**
+     * DO NOT USE FOR DATE RANGES THAT ARE MORE THAN 1 WEEK AGO! <br>
+     * Events are only stored by the system for a few days.
+     *
+     * @param startTime Start time of evaluation in milliseconds
+     * @param endTime   End time of evaluation in milliseconds
+     * @return Map of package names to usage time in milliseconds for the given time range
+     */
+    private Map<String, Long> getUsageTimeForRange(long startTime, long endTime) {
         UsageEvents usageEvents = statsManager.queryEvents(startTime, endTime);
         Map<String, List<Event>> eventsByPackage = new HashMap<>();
 
@@ -143,26 +161,31 @@ public class AppTimeService extends Service{
                     && currentEvent.getEventType() != Event.ACTIVITY_PAUSED) {
                 continue;
             }
-            if (packageNames.contains(packageName)) {
+            if (allPackageNames.contains(packageName)) {
                 eventsByPackage.putIfAbsent(packageName, new ArrayList<>());
                 Objects.requireNonNull(eventsByPackage.get(packageName)).add(currentEvent);
             }
         }
 
-        return getUsageTimeFromEvents(packageNames, eventsByPackage);
+        return getUsageTimeFromEvents(allPackageNames, eventsByPackage);
     }
 
     @NonNull
-    private Map<String, Integer> getUsageTimeFromEvents(List<String> packageNames, Map<String, List<Event>> eventsByPackage) {
-        Map<String, Integer> usageTimeMap = new HashMap<>();
+    private List<String> getAllPackageNames() {
+        return packageManager.getInstalledApplications(0)
+                .stream()
+                .filter(applicationInfo -> (applicationInfo.flags & ApplicationInfo.FLAG_SYSTEM) != 1)
+                .map(applicationInfo -> applicationInfo.packageName)
+                .collect(Collectors.toList());
+    }
+
+    @NonNull
+    private Map<String, Long> getUsageTimeFromEvents(List<String> packageNames, Map<String, List<Event>> eventsByPackage) {
+        Map<String, Long> usageTimeMap = new HashMap<>();
         for (String packageName : packageNames) {
             String userFriendlyAppName;
-            try {
-                userFriendlyAppName = packageManager.getApplicationLabel(packageManager.getApplicationInfo(packageName, 0)).toString();
-            } catch (PackageManager.NameNotFoundException e) {
-                userFriendlyAppName = packageName;
-            }
-            int time = getTotalTime(Objects.requireNonNull(eventsByPackage.getOrDefault(packageName, new ArrayList<>())));
+            userFriendlyAppName = getUserFriendlyAppName(packageName);
+            long time = getTotalTimeEvents(Objects.requireNonNull(eventsByPackage.getOrDefault(packageName, new ArrayList<>())));
             if (time > 0) {
                 usageTimeMap.put(userFriendlyAppName, time);
             }
@@ -170,8 +193,19 @@ public class AppTimeService extends Service{
         return usageTimeMap;
     }
 
-    private static int getTotalTime(List<Event> allEvents) {
-        int totalTime = 0;
+    @NonNull
+    private String getUserFriendlyAppName(String packageName) {
+        String userFriendlyAppName;
+        try {
+            userFriendlyAppName = packageManager.getApplicationLabel(packageManager.getApplicationInfo(packageName, 0)).toString();
+        } catch (PackageManager.NameNotFoundException e) {
+            userFriendlyAppName = packageName;
+        }
+        return userFriendlyAppName;
+    }
+
+    private static long getTotalTimeEvents(List<Event> allEvents) {
+        long totalTime = 0;
         for (int i = 0; i < allEvents.size() - 1; i++) {
             Event E0 = allEvents.get(i);
             Event E1 = allEvents.get(i + 1);
@@ -179,7 +213,7 @@ public class AppTimeService extends Service{
             if (E0.getEventType() == Event.ACTIVITY_RESUMED
                     && E1.getEventType() == Event.ACTIVITY_PAUSED
                     && E0.getClassName().equals(E1.getClassName())) {
-                totalTime += (int) (E1.getTimeStamp() - E0.getTimeStamp());
+                totalTime += (E1.getTimeStamp() - E0.getTimeStamp());
             }
         }
         return totalTime;
--- app/src/main/java/com/os/timeguardian/ui/openings/OpeningsFragment.java
+++ app/src/main/java/com/os/timeguardian/ui/openings/OpeningsFragment.java
@@ -1,12 +1,6 @@
 package com.os.timeguardian.ui.openings;
 
-import android.app.usage.UsageStats;
-import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.content.pm.ApplicationInfo;
-import android.content.pm.PackageManager;
-import android.content.pm.PackageManager.NameNotFoundException;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -17,18 +11,11 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
-import com.os.timeguardian.R;
 import com.os.timeguardian.backend.service.AppTimeService;
 import com.os.timeguardian.databinding.FragmentOpeningsBinding;
 
-import java.time.Instant;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.ZoneId;
-import java.util.Calendar;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 public class OpeningsFragment extends Fragment {
     private AppTimeService service;
@@ -37,9 +24,7 @@ public class OpeningsFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        String appName = getString(R.string.app_name);
-        SharedPreferences sharedPreferences = requireContext().getSharedPreferences(appName, Context.MODE_PRIVATE);
-        service = new AppTimeService(getContext());
+        service = new AppTimeService(requireContext());
 
         OpeningsViewModel openingsViewModel =
                 new ViewModelProvider(this).get(OpeningsViewModel.class);
@@ -51,10 +36,10 @@ public class OpeningsFragment extends Fragment {
         openingsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
 
         requireActivity().startService(new Intent(requireContext(), AppTimeService.class));
-        PackageManager packageManager = requireContext().getPackageManager();
 
-        List<Map<String, Integer>> usageStatsPastSevenDays = service.getUsageStatsPastSevenDays();
-        System.out.println(usageStatsPastSevenDays);
+        Map<String, Long> usageStatsToday = service.getUsageStatsToday();
+        List<Map<String, Long>> usageStatsPastSevenDays = service.getUsageStatsPastSevenDays();
+        List<Map<String, Long>> usageStatsTodayGroupByHours = service.getUsageStatsTodayGroupByHours();
 
         return root;
     }
