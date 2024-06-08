606526e19434faf0993631e602ce00962cd74297
Luca Gloor <glooorluca@gmail.com>
2024-06-08 14:59:11 +0200

    Add PackageUtil

---
+++ app/src/main/java/com/os/timeguardian/utils/PackageUtil.java
@@ -0,0 +1,33 @@
+package com.os.timeguardian.utils;
+
+import android.content.Context;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+
+import androidx.annotation.NonNull;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class PackageUtil {
+    public static List<String> getAllPackageNames(Context context) {
+        return context.getPackageManager()
+                .getInstalledApplications(0)
+                .stream()
+                .filter(applicationInfo -> (applicationInfo.flags & ApplicationInfo.FLAG_SYSTEM) != 1)
+                .map(applicationInfo -> applicationInfo.packageName)
+                .collect(Collectors.toList());
+    }
+
+    @NonNull
+    private String getUserFriendlyAppName(String packageName, Context context) {
+        PackageManager packageManager = context.getPackageManager();
+        String userFriendlyAppName;
+        try {
+            userFriendlyAppName = packageManager.getApplicationLabel(packageManager.getApplicationInfo(packageName, 0)).toString();
+        } catch (PackageManager.NameNotFoundException e) {
+            userFriendlyAppName = packageName;
+        }
+        return userFriendlyAppName;
+    }
+}
