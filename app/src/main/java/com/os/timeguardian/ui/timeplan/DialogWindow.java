824bb7de862fafe01ad25faf71736ea69873c581
Luca Gloor <glooorluca@gmail.com>
2024-06-11 22:04:29 +0200

    DialogWindow: Remove unused imports

--- app/src/main/java/com/os/timeguardian/ui/timeplan/DialogWindow.java
+++ app/src/main/java/com/os/timeguardian/ui/timeplan/DialogWindow.java
@@ -1,14 +1,12 @@
 package com.os.timeguardian.ui.timeplan;
 
 
-import android.annotation.SuppressLint;
 import android.app.Dialog;
 import android.content.Context;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.CheckBox;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
@@ -20,7 +18,6 @@ import com.os.timeguardian.R;
 import com.os.timeguardian.databinding.FragmentTimeplanBinding;
 import com.os.timeguardian.utils.PackageUtil;
 
-import java.util.Arrays;
 import java.util.List;
 
 public class DialogWindow {
