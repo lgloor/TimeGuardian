ab8bf65232fa8382ad64d9d51caa83fe14e31a75
Luca Gloor <glooorluca@gmail.com>
2024-06-11 21:46:34 +0200

    BackgroundService: Small refactoring

--- app/src/main/java/com/os/timeguardian/backend/service/BackgroundService.java
+++ app/src/main/java/com/os/timeguardian/backend/service/BackgroundService.java
@@ -6,8 +6,8 @@ import android.content.Intent;
 import android.media.AudioManager;
 import android.os.Handler;
 import android.os.IBinder;
-import android.util.Log;
 import android.util.Pair;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 
@@ -26,22 +26,16 @@ public class BackgroundService extends Service {
     private Long currentTime;
     private BrightnessHelperNew brightnessHelper;
     private VolumeHelper volumeHelper;
-    private NotificationHelper notificationHelper;
-    private int tracker;
-    private String lastUsedApp;
     private AppTimeService appTimeService;
 
     public BackgroundService(Context context, FileHelper fileHelper) {
-        tracker = 0;
         this.context = context;
         handler = new Handler();
         currentApp = "nothing";
-        lastUsedApp = "nothing";
         this.fileHelper = fileHelper;
         appTimeService = new AppTimeService(context);
         brightnessHelper = new BrightnessHelperNew(context);
         volumeHelper = new VolumeHelper(context);
-        notificationHelper = new NotificationHelper(context);
         startTracking();
     }
 
@@ -49,37 +43,23 @@ public class BackgroundService extends Service {
         runnable = new Runnable() {
             @Override
             public void run() {
-                /*Toast.makeText(context, "Hintergrundprozess läuft.", Toast.LENGTH_SHORT).show();
-                Log.e("Hintergrundprozess", "Läuft");
-                handler.postDelayed(this, 5000);*/
-                //currentApp = "Docs"; //TODO: currentApp aktualisieren mit aktuell genutzter App.
                 Pair<String, Long> usagePair = appTimeService.getUsageTimeOfCurrentForegroundForToday();
                 currentApp = PackageUtil.getUserFriendlyAppName(usagePair.first, context);
                 currentTime = usagePair.second;
-                System.out.println(currentTime);
-                HashMap<String, String> cmp = getPunishmentEntries();
-                if (cmp.containsKey(currentApp) && currentApp != null) {
-                    String punishment = cmp.get(currentApp);
-                    if (!lastUsedApp.equals(currentApp)) {
-                        tracker = 0;
-                    }
-                    lastUsedApp = currentApp;
-                    tracker+=5;
-                    Log.e("Aktueller Zeitmesser", String.valueOf(tracker));
-                    if (currentTime>=30000 && punishment.equals("Soft")) {
-                        notificationHelper.sendHighPriorityNotification("Soft punishment", "Maybe take a look at your screen time.");
-                        tracker = 0;
-                    } else if (currentTime>=30000 && punishment.equals("Middle")) {
-                        notificationHelper.sendHighPriorityNotification("Middle punishment", "That is too much screen time. ");
+                HashMap<String, String> punishmentEntries = getPunishmentEntries();
+                if (punishmentEntries.containsKey(currentApp) && currentApp != null) {
+                    String punishment = punishmentEntries.get(currentApp);
+                    if (currentTime>=30000 && "Soft".equals(punishment)) {
+                        Toast.makeText(context, "Maybe take a look at your screen time.", Toast.LENGTH_SHORT).show();
+                    } else if (currentTime>=30000 && "Middle".equals(punishment)) {
+                        Toast.makeText(context, "That is too much screen time.", Toast.LENGTH_SHORT).show();
                         brightnessHelper.requestWriteSettingsPermission();
                         brightnessHelper.setSystemBrightness(0);
-                        tracker = 0;
-                    } else if(currentTime>=30000 && punishment.equals("Hard")) {
-                        notificationHelper.sendHighPriorityNotification("Hard punishment", "YOU GET THE ULTIMATE PUNISH !!!");
+                    } else if(currentTime>=30000 && "Hard".equals(punishment)) {
+                        Toast.makeText(context, "YOU GET THE ULTIMATE PUNISH!!!", Toast.LENGTH_SHORT).show();
                         brightnessHelper.requestWriteSettingsPermission();
                         brightnessHelper.setSystemBrightness(0);
                         volumeHelper.setMaxVolume(AudioManager.STREAM_MUSIC);
-                        tracker = 0;
                     }
                 }
                 handler.postDelayed(this, 5000);
